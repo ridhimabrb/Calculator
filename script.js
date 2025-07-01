0c23d60204ad40ebfcaadc07dc2b6b7014e12a1a
ridhimabrb <ridhimapant@outlook.com>
2025-07-01 18:40:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate(operator) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Please enter valid numbers.";
+  } else {
+    switch (operator) {
+      case '+': result = num1 + num2; break;
+      case '-': result = num1 - num2; break;
+      case '*': result = num1 * num2; break;
+      case '/': 
+        result = num2 === 0 ? "Cannot divide by zero!" : (num1 / num2); 
+        break;
+    }
+  }
+
+  document.getElementById("result").innerText = "Result: " + result;
+}
